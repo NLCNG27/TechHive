6ab7d5ea306657f4d3eec4ebe7df2d3bc9da4780
Nguyen Lam <nguyen@cngsoftware.com>
2024-11-06 15:25:19 -0600

    Initial Commit

---
+++ TechHive/TechHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechHiveApp.swift
+//  TechHive
+//
+//  Created by Nguyen Lam on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TechHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
